cd6a627b6f7ee76b894d7a418e361f8a83379986
Illinux <snellemany@gmail.com>
2020-03-04 00:13:26 +0100

    Canine Initial commit; latest working from xander1998

--- config/main.lua
+++ config/main.lua
@@ -1,25 +1,44 @@
-canine = {}
-canine = setmetatable(canine, {})
+K9Config = {}
+K9Config = setmetatable(K9Config, {})
 
-canine.OpenMenuIdentifierRestriction = false
-canine.LicenseIdentifiers = {
-	""
+K9Config.OpenMenuIdentifierRestriction = true
+K9Config.OpenMenuPedRestriction = true
+K9Config.LicenseIdentifiers = {
+	"license:c06fbf1faaf995c7b9e207ef77712971a3ed4dc3"
 }
-canine.SteamIdentifiers = {
-	""
+K9Config.SteamIdentifiers = {
+	"steam:1100001081f9ab0"
+}
+K9Config.PedsList = {
+	"s_m_y_cop_01",
+	"s_m_y_sheriff_01"
 }
 
 -- Restricts the dog to getting into certain vehicles
-canine.VehicleRestriction = false
-canine.VehiclesList = {
+K9Config.VehicleRestriction = false
+K9Config.VehiclesList = {
+
 }
 
-canine.SearchType = "Random"
-canine.OpenDoorsOnSearch = true
+-- Searching Type ( RANDOM [AVAILABLE] | VRP [NOT AVAILABLE] | ESX [NOT AVAILABLE] )
+K9Config.SearchType = "Random"
+K9Config.OpenDoorsOnSearch = false
+
+-- Used for Random Search Type --
+K9Config.Items = {
+	{item = "Cocaine", illegal = true},
+	{item = "Marijuana", illegal = true},
+	{item = "Blunt Spray", illegal = false},
+	{item = "Crowbar", illegal = false},
+	{item = "Lockpicks", illegal = false},
+	{item = "Baggies", illegal = false},
+	{item = "Used Needle", illegal = false},
+	{item = "Open Container", illegal = false},
+}
 
 -- Language --
-canine.LanguageChoice = "English"
-canine.Languages = {
+K9Config.LanguageChoice = "English"
+K9Config.Languages = {
 	["English"] = {
 		follow = "Come",
 		stop = "Heel",
--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,6 +4,24 @@ game 'gta5'
 
 description 'Kowalski Canine'
 
+ui_page("html/index.html")
+
+files {
+    "html/index.html",
+    "html/libraries/axios.min.js",
+    "html/libraries/vue.min.js",
+    "html/libraries/vuetify.js",
+    "html/libraries/vuetify.css",
+    "html/style.css",
+    "html/script.js",
+    "html/images/dog_left.png",
+    "html/images/dog_right.png",
+    "html/images/husky.png",
+    "html/images/rottweiler.png",
+    "html/images/shepherd.png",
+    "html/images/retriever.png"
+}
+
 server_scripts {
 	'config/main.lua',
 	'server/main.lua'
